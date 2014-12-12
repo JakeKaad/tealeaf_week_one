defb12b11c4ea7356f13f2c57dac618e651a3cca
jakekaad <kaadalac@gmail.com>
2014-12-12 18:19:58 +0000

    refactored calculator per suggestions

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,15 @@
-operator = 'a'
 def say(msg)
   puts "~~~~" + msg + "~~~~"
 end
 
+def get_operator
+  operator = loop do  
+    say "Would you like to 1: Add, 2: Subtract, 3: Multiply, 4: divide"
+    operator = gets.chomp
+    return operator if ["1", "2", "3", "4"].include?(operator)
+  end
+end
+  
 say "Welcome to my calculator."
 say " "
 say "What is your first number?"
@@ -11,24 +18,17 @@ num1 = gets.chomp
 say "Great, and what is your second number?"
 num2 = gets.chomp
 
+operator = get_operator
 
-loop do  
-  say "Would you like to 1: Add, 2: Subtract, 3: Multiply, 4: divide"
-  operator = gets.chomp
-  break if operator == '1' || operator == '2' || 
-           operator == '3' || operator == '4'
-end 
-
-if operator == "1"  
-  result = num1.to_i + num2.to_i 
-elsif operator == "2" 
-  result = num1.to_i - num2.to_i
-elsif operator == "3" 
-  result = num1.to_i * num2.to_i
-elsif operator  == "4" 
-  result = num1.to_f / num2.to_f
-else 
-  "invalid operator"
+result = case operator
+  when "1"
+    num1.to_i + num2.to_i 
+  when "2" 
+    num1.to_i - num2.to_i
+  when "3" 
+    num1.to_i * num2.to_i
+  when "4" 
+    num1.to_f / num2.to_f
 end
 
 say "The result is #{result}"

047754751e9421de91acb463110faa84b115cfe3
jakekaad <kaadalac@gmail.com>
2014-12-14 17:57:17 +0000

    Finished Tic-Tac-Toe

--- tictactoe.rb
+++ tictactoe.rb
@@ -2,6 +2,8 @@
 require 'pry'
 #tic-tac-toe!
 claimed_squares = ["_", "_", "_", "_", "_", "_", "_", "_", "_"]
+positions = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
 
  
 def board(claimed_squares)
@@ -17,28 +19,71 @@ def board(claimed_squares)
 end
 
 
+def is_tie?(positions)
+  if positions.empty?
+    puts "Its a stalemate!"
+    return true
+  elsif positions.any?
+    return false
+  end
+end
+
+
+ def check_for_winner?(claimed_squares)
+   three_in_a_row = [[claimed_squares[0], claimed_squares[1], claimed_squares[2]],
+                     [claimed_squares[2], claimed_squares[5], claimed_squares[8]],
+                     [claimed_squares[6], claimed_squares[7], claimed_squares[8]],
+                     [claimed_squares[0], claimed_squares[3], claimed_squares[6]],
+                     [claimed_squares[0], claimed_squares[4], claimed_squares[8]],
+                     [claimed_squares[2], claimed_squares[4], claimed_squares[6]],
+                     [claimed_squares[1], claimed_squares[4], claimed_squares[7]],
+                     [claimed_squares[3], claimed_squares[4], claimed_squares[5]]]
+    if three_in_a_row.include?(["x", "x", "x"])
+      puts "3 in a row! You win!"
+      return true
+    elsif three_in_a_row.include?(["o", "o", "o"])
+      puts "3 in a row! You lose!"
+      return true
+    else
+      return false
+    end
+  end
+  
+  def check_for_resolution?(claimed_squares, positions)
+    if check_for_winner?(claimed_squares) || is_tie?(positions)
+      board(claimed_squares)
+      return true
+    else
+      return false
+    end
+  end
+
 def choose_a_square
   square_to_claim = gets.chomp.to_i
   square_to_claim -= 1
 end
 
-
 puts "Lets play tic-tac-toe!"
 
 loop do 
+ 
   board(claimed_squares)
+  
   begin
     puts "Please choose an empty square using [1-9]"
-    player_square = claimed_squares[choose_a_square]
-    binding.pry
-    if player_square == '_'
-      player_square = "x"
-    else
-      "Please choose an empty square"
-    end
-    binding.pry
-  end while player_square == "x"
+    player_square = choose_a_square
+  end until positions.include?(player_square)
   
+  player_square = positions.shuffle!.delete(player_square)
+  claimed_squares[player_square] = 'x'
+  
+  break if check_for_resolution?(claimed_squares, positions)
+ 
+  computer_square = positions.pop
+  claimed_squares[computer_square] = 'o'
+  
+  break if check_for_resolution?(claimed_squares, positions)
+ 
   
   
 end
